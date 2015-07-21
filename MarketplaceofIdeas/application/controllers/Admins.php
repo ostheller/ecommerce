94b0acb0b8b519fa2c2b02a0fc26a27904f7a167
Aadi <aadi.nickels@gmail.com>
2015-07-21 12:42:44 -0700

    database file and admins controller
    added username and pass to the db file and added function to admins

--- MarketplaceofIdeas/application/controllers/Admins.php
+++ MarketplaceofIdeas/application/controllers/Admins.php
@@ -1,10 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admins extends CI_Controller {
+class Admins extends CI_Controller 
+{
 	public function index()
 	{
 		$this->load->view('admin_login');
 	}
+	public function login()
+	{
+
+	}	
 }
 ?>
