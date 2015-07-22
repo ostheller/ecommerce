b1c30cd890f433d866cda74fe34a1071b6737fc8
HaggardSage <jess.patton1@comcast.net>
2015-07-21 17:41:44 -0700

    Added comments to the Users_Ideas controller file
    Added comments on each of the functions within the User_Ideas
    controller page.

--- MarketplaceofIdeas/application/controllers/users_ideas.php
+++ MarketplaceofIdeas/application/controllers/users_ideas.php
@@ -4,23 +4,46 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users_ideas extends CI_Controller {
 
 	
+	/* This function should only be called with the initial visit to the site */
 	public function index()
 	{
 		$this->load->view('user_landing');
 	}
 
+
+	/* EVENTS to trigger this function should be:
+		Keyword search
+		Category selection on Browsing page
+		Go Back link on User_Show Page
+		Question - Should the Go Back link go back to the same page as the show item
+					was displayed on or do we refresh that category / keyword list? 
+					Need to figure out how to impliment javascript history*/
 	public function browsing()
 	{
 		$this->load->view('user_browsing');
 	}
 
+
+
+	/*  EVENTS to trigger this function should be:
+		Featured idea on User_Landing Page
+		Thumbnail image on User_Browser Page
+		Similar Items on the User_Show Page   */
 	public function show()
 	{
 		$this->load->view('user_show');
 	}
 
+
+	/*I wonder if the cart should be part of the orders controller instead of the ideas.
+		EVENTS to trigger this function should be:
+		Shopping Cart on User_Landing Page, 
+		Shopping Cart on User_Browser Page,  
+
+				MOVED TO ORDERS CONTROLLER FILE
 	public function cart()
 	{
 		$this->load->view('user_cart');
 	}
+	*/
 }
