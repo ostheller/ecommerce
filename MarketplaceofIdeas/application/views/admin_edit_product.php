07d6bc0e9417b0d80a07755955361bc6b0486a8b
Aadi <aadi.nickels@gmail.com>
2015-07-22 08:17:41 -0700

    admin_edit_product
    prelim img upload and commenting on file ends

--- MarketplaceofIdeas/application/views/admin_edit_product.php
+++ MarketplaceofIdeas/application/views/admin_edit_product.php
@@ -29,31 +29,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<option><?=//$tag?></option>
 			<?//}?>
 		</select>
-		<!-- when the admin hovers over the <option>s an edit and delete icon need to appear and function WHEN option edit is clicked the <option> is replaced with <input type='text'> -->
-		<!-- when DELETE is clicked on display alert that confirms deletion of category -->
+<!-- when the admin hovers over the <option>s an edit and delete icon need to appear and function WHEN option edit is clicked the <option> is replaced with <input type='text'> -->
+<!-- when DELETE is clicked on display alert that confirms deletion of category -->
 		<form action='addTag' method='post'>
-		<label>or add new tag:</label><input type='text' name='addTag'>
-		<!-- tog selection field: should it update upon moving out of input field OR does the tag just get added to the DB -->
-		<label>which category does this tag belong to?</label>
-		<select>
-			<?//foreach($categories as $category)
-			//{?>
-				<option><?=//$category?></option>
-			<?//}?>
-		</select>
-		<input type='submit' value='Add New Tag'>
-	</form>
+			<label>or add new tag:</label><input type='text' name='addTag'>
+<!-- tog selection field: should it update upon moving out of input field OR does the tag just get added to the DB -->
+			<label>which category does this tag belong to?</label>
+			<select>
+				<?//foreach($categories as $category)
+				//{?>
+					<option><?=//$category?></option>
+				<?//}?>
+			</select>
+			<input type='submit' value='Add New Tag'>
+		</form><!--end of addTag form-->
 		<div id='assocTags'>
-			<!-- populate with all the tags associated with the product AJAX to add any new tags without full refresh  -->
+<!-- populate with all the tags associated with the product AJAX to add any new tags without full refresh  -->
 		</div>
-			<form action='imgUpload' method='post'>
-				<label>Images</label>
-				<input type='file' name='image'>
-				<input type='submit' value='Upload'>
-				<!-- the upload button is the browse button (ie to find the img file you want to upload) -->
-			</form>
-			<div>
-				<form action='setImg' method='post'>
+		<form action='imgUpload' method='post' enctype='multipart/form-data' >
+			<label>Images</label>
+			<input type='file' name='imageToUpload'>
+			<input type='submit' value='upload'>
+<!-- the upload button is the browse button (ie to find the img file you want to upload) -->
+		</form><!-- end of imgUpload form-->
+		<div>
+			<form action='setImg' method='post' enctype='multipart/form-data'>
 				<?//foreach($image)
 				//<!--the idea is that the for each will populate each image with the appropriate thumbnail and the delete and main checkbox  -->
 				//{?>
@@ -61,13 +61,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<!-- make sure that only one box can be checked as main -->
 					<!-- wire frame mentions that trash/delete and dragability is hidden until you hover on the image AND we need make the image dragable -->
 				<?//}?>
-				</form>
+			</form><!-- end of setImg form-->
 			</div>
 		<button>Cancel</button>
 		<!-- redirects to the admin product dash -->
 		<button>Preview</button>
 		<!-- preview button opens a new tab with the product page displaying appropriate info displayed -->
 		<input type='submit' value='Update'>
-	</form>
+	</form><!--end of edit form-->
 </body>
 </html>
