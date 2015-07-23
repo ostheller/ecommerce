7d95fb094c29570dac18f6a306e201f6d8af940c
Molly Ostheller <molly.ostheller@gmail.com>
2015-07-22 21:12:26 -0700

    More merge things

--- MarketplaceofIdeas/application/views/user_browsing.php
+++ MarketplaceofIdeas/application/views/user_browsing.php
@@ -1,150 +1,24 @@
-<html>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-		<meta charset='utf-8'>
-		<title>User Browser</title>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Browse Ideas</title>
+	    <script src="<?= base_url();?>/assets/js/jquery.js"></script>
+	    <script src="<?= base_url();?>/assets/js/bootstrap.min.js"></script>
+		<link href="<?= base_url();?>/assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+		<link href="<?= base_url();?>/assets/css/modern-business.css" rel="stylesheet" type="text/css">
+		<link href="<?= base_url();?>/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 		<style type="text/css">
-
-		.menu{
-			width: 200px;
-			display: inline-block;
-			border: 1px solid silver;
-		}
-
-			.menu a{
-				display: block;
-			}
-
-		.results{
-			width: 900px;
-			vertical-align: top;
-			display: inline-block;
-		}
-			.results h3{
-				width: 400px;
-				display: inline-block;
-			}
-
-			.results a{
-				margin-left: 25px;
-			}
-
-			.results b{
-				margin-left: 25px;
-			}
-
-			.results label{
-				margin-left: 20px;
-			}
-
-		.item{
-			width: 200px;
-			display: inline-block;
-		}
-
-		.pages{
-			width: 1200px;
-			margin-left: 25px;
-			text-align: center;
-		}
-
-		.allitems{
-			width: 900px;
-			display: inline-block;
-			vertical-align: top;
-		}
-
+			/** {
+				outline: 1px solid purple;
+			}*/
 		</style>
-
 	</head>
-
 	<body>
-<<<<<<< Updated upstream
-	<!-- <header>!LOAD PARTIAL/NAVBAR</header> -->
-			<h3>Marketplace of Ideas</h3>
-		<div class='menu'>
-			<form action ='' method='post'>
-				<input type='text' value='keyword'>
-				<input type='hidden' value='search'>
-				<input type='submit' value="Search">
-				<h5>Categories</<h5>
-				<a href="">Western</a>
-				<a href="">Eastern</a>
-				<a href="">Ancient Greek</a>
-				<a href="">Modern</a>
-				<a href="">Post-Modern</a>
-			</form>
-		</div>
-		<div class='results'>
-			<div class = 'allproucts'>
-				<h3>Philosophies (page 1)</h3>
-				<a href="">first</a>
-				<a href="">prev</a>
-				<b>2</b>
-				<a href="">next</a>
-				<label>Sort by</label>
-				<select>
-					<option>Price</option>
-					<option>Most Popular</option>
-					<option>Newest</option>
-				</select>
-			</div>
-
-			<div class= 'allitems'>
-				<div class = 'item'>
-						<img src="http://0.tqn.com/d/ancienthistory/1/0/h/P/Socrates.jpg" width="200" height="200" >
-				</div>
-				<div class = 'item'>
-						<img src="http://www.universaltheosophy.com/wp-content/gallery/biography-pictures/500-pythagoras_240x360.png" width="200" height="200">
-				</div>
-				<div class = 'item'>
-						<a href="/ideas/show"><img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="200" height="200"></a>	
-				</div>
-				<div class = 'item'>
-						<img src="http://www.philosophers.co.uk/immanuel-kant.jpg" width="200" height="200">
-				</div>
-				<div class = 'item'>
-						<img src="http://0.tqn.com/d/ancienthistory/1/0/h/P/Socrates.jpg" width="200" height="200" >
-				</div>
-				<div class = 'item'>
-						<img src="http://www.universaltheosophy.com/wp-content/gallery/biography-pictures/500-pythagoras_240x360.png" width="200" height="200">
-				</div>
-				<div class = 'item'>
-						<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="200" height="200">	
-				</div>
-				<div class = 'item'>
-						<img src="http://www.philosophers.co.uk/immanuel-kant.jpg" width="200" height="200">
-				</div>
-				<div class = 'item'>
-						<img src="http://0.tqn.com/d/ancienthistory/1/0/h/P/Socrates.jpg" width="200" height="200" >
-				</div>
-				<div class = 'item'>
-						<img src="http://www.universaltheosophy.com/wp-content/gallery/biography-pictures/500-pythagoras_240x360.png" width="200" height="200">
-				</div>
-				<div class = 'item'>
-						<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="200" height="200">	
-				</div>
-				<div class = 'item'>
-						<img src="http://www.philosophers.co.uk/immanuel-kant.jpg" width="200" height="200">
-				</div>
-				<div class = 'item'>
-						<img src="http://0.tqn.com/d/ancienthistory/1/0/h/P/Socrates.jpg" width="200" height="200" >
-				</div>
-				<div class = 'item'>
-						<img src="http://www.universaltheosophy.com/wp-content/gallery/biography-pictures/500-pythagoras_240x360.png" width="200" height="200">
-				</div>
-				<div class = 'item'>
-						<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="200" height="200">	
-				</div>
-				<div class = 'item'>
-						<img src="http://www.philosophers.co.uk/immanuel-kant.jpg" width="200" height="200">
-				</div>
-			</div>
-		</div>
-
-		<div class='pages'>
-			<p><a href=""><<</a>  |  <a href="">1</a>  |  2  |  <a href="">3</a>  |  <a href="">4</a>  |  <a href="">5</a> | <a href="">>> </a></p>
-		</div>
-=======
 		<?php $this->load->view('partials/user_navbar'); ?>
 
 		<!-- Page Content -->
@@ -235,7 +109,6 @@
             </div>
         </div>
         <!-- /.row -->
->>>>>>> Stashed changes
 
 		
 
--- MarketplaceofIdeas/application/views/user_show.php
+++ MarketplaceofIdeas/application/views/user_show.php
@@ -1,85 +1,3 @@
-<<<<<<< Updated upstream
-<html>
-	<head>
-		<meta chartset='utf-8'>
-		<title>User Show Details</title>
-		<style type="text/css">
-
-		*{
-			outline: 1px dotted red;
-		}
-
-		.images{
-			width: 250px;
-			display: inline-block;
-		}
-
-		.smallimage{
-			margin-top: 5px;
-			display: inline-block;
-		}
-
-		.description{
-			margin-left: 25px;
-			width: 700px;
-			vertical-align: top;
-			display: inline-block;
-		}
-
-			.description form{
-				margin-left: 450px;
-			}
-
-		.suggestions{
-			margin: 20 30px;
-			display: inline-block;
-		}
-
-			.suggestions p{
-				width: 150px;
-				text-align: center;
-				display: inline-block;
-
-			}
-
-
-		</style>
-	</head>
-	<body>
-		<h4>Partial goes here</h4>
-		<a href="/">Go Back</a>
-
-		<h2>Philosophies of C. West</h2>
-
-		<div class='images'>
-			<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="250" height="250">	
-			<div class='smallimage'>
-				<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="40" height="40">	
-				<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="40" height="40">	
-				<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="40" height="40">	
-				<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="40" height="40">	
-				<img style="-webkit-user-select: none;" src="http://www.toptenz.net/wp-content/uploads/2014/10/americanphilosophers4-640x480.jpg" width="40" height="40">	
-			</div>
-		</div>
-
-		<div class='description'>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-			 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-			  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-			  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-			  mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-			  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-			  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-			   esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-			    qui officia deserunt mollit anim id est laborum.</p>
-
-			    <form action='/users_orders/cart' method='post'>
-			    	<select name='quantity'>
-			    		<option value='19.95'>1</option>
-			    		<option value='37.95'>2</option>
-			    		<option value='55.95'>3</option>
-			    		<option value='73.95'>4</option>
-=======
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 // echo "<pre>";
@@ -164,26 +82,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			    		<option>2($37.95)</option>
 			    		<option>3($55.95)</option>
 			    		<option>4($73.95)</option>
->>>>>>> Stashed changes
 			    	</select>
-			    	<input type='hidden' name='add'>
+			    	<input type='hidden' value='add'>
 			    	<input type='submit' value='Add to Cart'>
 			    </form>
-<<<<<<< Updated upstream
-
-			</div>
-
-		<div class='suggestions'>
-			<h3>Similar items</h3>
-			<img src="http://www.philosophers.co.uk/immanuel-kant.jpg" width="150" height="150">
-			<img src="http://0.tqn.com/d/ancienthistory/1/0/h/P/Socrates.jpg" width="150" height="150" >
-			<img src="http://www.universaltheosophy.com/wp-content/gallery/biography-pictures/500-pythagoras_240x360.png" width="150" height="150">	
-			<br>
-			<p>Kant</p><p>Socrates</p><p>Pythagoras</p>
-		</div>
-
-
-=======
             </div>
 
         </div>
@@ -207,6 +109,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <?php } } ?>
         </div>
         <!-- /.row -->
->>>>>>> Stashed changes
 	</body>
 </html>
