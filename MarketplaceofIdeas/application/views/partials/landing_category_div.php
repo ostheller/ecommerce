21df650eb34d0917023c0690c3b3f1e2351bb281
Molly Ostheller <molly.ostheller@gmail.com>
2015-07-23 19:27:02 -0700

    Dummy pictures

--- MarketplaceofIdeas/application/views/partials/landing_category_div.php
+++ MarketplaceofIdeas/application/views/partials/landing_category_div.php
@@ -1,7 +1,7 @@
 <?php foreach($data as $datum) { ?>
     <div class="col-md-4 img-portfolio">
         <a href="/browse/<?= $datum['name'] ?>">
-            <img class="img-responsive img-hover" src="<?= base_url();?>/assets/img/<?= $datum['location'] ?>" alt="<?= $datum['alt_text'] ?>">
+            <img class="img-responsive img-hover" src="<?= base_url();?>/assets/img/socrates.png" alt="<?= $datum['alt_text'] ?>">
         </a>
         <h4 class='text-center' >
             <a href="/browse/<?= $datum['name'] ?>"><?= $datum['name'] ?></a>
